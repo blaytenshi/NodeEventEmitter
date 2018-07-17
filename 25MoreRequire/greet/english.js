f787e9efa66a747249e7d0641e0f91805a1e6c1d
Jimmy Tang <blay.tenshi@gmail.com>
2018-07-18 01:05:07 +1000

    LESSON 25 MORE REQUIRE (UPDATED)

--- 25MoreRequire/greet/english.js
+++ 25MoreRequire/greet/english.js
@@ -1,5 +1,8 @@
+// Node will automatically convert the JSON object into a regular javascript object when you require a JSON file
+var greetings = require('./greetings.json');
+
 var greet = function() {
-    console.log('Hello')
+    console.log(greetings.en) // ...the imported JSON file turned into an object with accessible properties! WOW!
 };
 
 // exporting a variable which is a function, nothing we haven't seen already
--- 25MoreRequire/greet/spanish.js
+++ 25MoreRequire/greet/spanish.js
@@ -1,5 +1,8 @@
+// Node will automatically convert the JSON object into a regular javascript object when you require a JSON file
+var greetings = require('./greetings.json');
+
 var greet = function() {
-    console.log('Hola')
+    console.log(greetings.es) // ...the imported JSON file turned into an object with accessible properties! WOW!
 };
 
 // exporting a variable which is a function, nothing we haven't seen already
