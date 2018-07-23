41f921bbc612a7f16c3216332430d1994280a2fd
Jimmy Tang <blay.tenshi@gmail.com>
2018-07-24 01:50:10 +1000

    LESSON 37 - Node, ES6 and Template Literals

---
+++ 37NodeES6TemplateLiterals/app.js
@@ -0,0 +1,8 @@
+// This was written with node 10.1.0 installed! If you don't have it, you'll need to use Babel to transpile it to regular ES5
+var name = 'John Doe';
+
+var greet = 'Hello ' + name;
+var greet2 = `Hello ${name}`;
+
+console.log(greet);
+console.log(greet2);
